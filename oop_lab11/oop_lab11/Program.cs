230414527c184d1d4c62ec922a9be6c9a52e068c
alexandra komkova <sashakomkova2001@gmail.com>
2020-11-16 21:38:54 +0300

    lalala

--- oop_lab11/oop_lab11/Program.cs
+++ oop_lab11/oop_lab11/Program.cs
@@ -123,14 +123,14 @@ namespace oop_lab11
         {
             string[] m = new [] { "December", "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November" };
             int n = 7;
-            var selectedN = from t in m // определяем каждый объект из teams как t
+            var selectedN = from t in m // определяем каждый объект из m как t
                                 where t.Length == n //фильтрация по критерию
                                 select t; // выбираем объект
             foreach (string s in selectedN)
                 Console.WriteLine(s);
             Console.WriteLine("\n");
 
-            var selectedSummerOrWinter = from t in m // определяем каждый объект из teams как t
+            var selectedSummerOrWinter = from t in m // определяем каждый объект из m как t
                             where t == "December" || t== "January" || t == "February" || t == "June" || t == "July" || t == "August"
                                          orderby t
                                          select t; // выбираем объект
@@ -138,16 +138,16 @@ namespace oop_lab11
                 Console.WriteLine(s);
             Console.WriteLine("\n");
 
-            var selectedAlphabet = from t in m // определяем каждый объект из teams как t
+            var selectedAlphabet = from t in m 
                                          orderby t
-                                         select t; // выбираем объект
+                                         select t; 
             foreach (string s in selectedAlphabet)
                 Console.WriteLine(s);
             Console.WriteLine("\n");
 
-            var selectedUN = from t in m // определяем каждый объект из teams как t
-                            where t.Length > 4 && t.Contains("u")    //фильтрация по критерию
-                            select t; // выбираем объект
+            var selectedUN = from t in m 
+                            where t.Length > 4 && t.Contains("u")    
+                            select t; 
             foreach (string s in selectedUN)
                 Console.WriteLine(s);
             Console.WriteLine("\n");
@@ -189,7 +189,7 @@ namespace oop_lab11
             Console.WriteLine("\n");
 
             
-            int old = listCars.Max(a => a.car_age); //??????
+            int old = listCars.Max(a => a.car_age); 
             var selectedOld = from t in listCars 
                               where t.car_age == old
                                          select t; 
---
+++ oop_lab13/oop_lab13/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop_lab13
+{
+    public class KAVLog 
+    {
+        StreamWriter sw = new StreamWriter(@"D:\Саша Комкова\oop\oop_lab13\kavlog.txt", true);
+        public void Log()
+        {
+        }
+
+    }
+    public static class KAVDriveInfo
+    {
+        public static void DriveInfoWrite() 
+        {
+            StreamWriter sw = new StreamWriter(@"D:\Саша Комкова\oop\oop_lab13\kavlog.txt", true);
+            DriveInfo[] drives = DriveInfo.GetDrives();
+            foreach (DriveInfo drive in drives)
+            {
+                Console.WriteLine($"название: {drive.Name}");
+                Console.WriteLine($"тип: {drive.DriveType}");
+                if (drive.IsReady)
+                {
+                    Console.WriteLine($"объем диска: {drive.TotalSize}");
+                    Console.WriteLine($"свободное пространство: {drive.TotalFreeSpace}");
+                    Console.WriteLine($"метка: {drive.VolumeLabel}");
+                }
+                Console.WriteLine();
+            }
+        }
+       
+
+
+    }
+    public static class KAVFileInfo
+    {
+        public static void FileInfoWrite()
+        { 
+           //StreamWriter sw = new StreamWriter(@"D:\Саша Комкова\oop\oop_lab13\kavlog.txt", true);
+            string path = @"D:\Саша Комкова\oop\oop_lab13\kavlog.txt";
+            FileInfo fileInf = new FileInfo(path);
+            if (fileInf.Exists)
+            {
+                Console.WriteLine("полный путь: {0}", fileInf.DirectoryName);
+                Console.WriteLine("размер: {0}", fileInf.Length);
+                Console.WriteLine("расширение: {0}", fileInf.Extension);
+                Console.WriteLine("имя файла: {0}", fileInf.Name);
+                Console.WriteLine("время создания: {0}", fileInf.CreationTime);
+                Console.WriteLine("время изменения: {0}", fileInf.LastWriteTime);
+
+            }
+
+        }
+
+
+
+    }
+    public static class KAVDirInfo
+    {
+        public static void DirInfoWrite()
+        {
+            StreamWriter sw = new StreamWriter(@"D:\Саша Комкова\oop\oop_lab13\kavlog.txt", true);
+            string dirName = @"D:\Саша Комкова\oop";
+            if (Directory.Exists(dirName))
+            {
+                Console.WriteLine("подкаталоги:");
+                string[] dirs = Directory.GetDirectories(dirName);
+                foreach (string s in dirs)
+                {
+                    Console.WriteLine(s);
+                }
+                Console.WriteLine();
+                Console.WriteLine("файлы:");
+                string[] files = Directory.GetFiles(dirName);
+                foreach (string s in files)
+                {
+                    Console.WriteLine(s);
+                }
+                DirectoryInfo dirInfo = new DirectoryInfo(dirName);
+                Console.WriteLine($"родительский каталог: {dirInfo.Parent}");
+                Console.WriteLine($"время создания: {dirInfo.CreationTime}");
+
+            }
+
+        }
+
+
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            KAVDriveInfo.DriveInfoWrite();
+            KAVFileInfo.FileInfoWrite();
+            KAVDirInfo.DirInfoWrite();
+
+
+
+            Console.ReadKey();
+        }
+
+    }
+}
