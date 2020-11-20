58e6fc6b64663efabe937453e4ad1d36f1a48e0f
alexandra komkova <sashakomkova2001@gmail.com>
2020-11-21 00:32:03 +0300

    new

--- oop_lab13/oop_lab13/Program.cs
+++ oop_lab13/oop_lab13/Program.cs
@@ -1,17 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.IO.Compression;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace oop_lab13
 {
-    public class KAVLog 
+    public static class KAVLog 
     {
-        StreamWriter sw = new StreamWriter(@"D:\Саша Комкова\oop\oop_lab13\kavlog.txt", true);
-        public void Log()
+        public static void Log()
         {
+            
+            string sw = @"D:\Саша Комкова\oop\oop_lab13\kavlogfile.txt";
+            try
+            {
+                using (StreamReader sr = new StreamReader(sw))
+                {
+                    Console.WriteLine(sr.ReadToEnd());
+                }
+                
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
 
     }
@@ -19,7 +33,7 @@ namespace oop_lab13
     {
         public static void DriveInfoWrite() 
         {
-            StreamWriter sw = new StreamWriter(@"D:\Саша Комкова\oop\oop_lab13\kavlog.txt", true);
+            StreamWriter sw = new StreamWriter(@"D:\Саша Комкова\oop\oop_lab13\kavlogfile.txt", true);
             DriveInfo[] drives = DriveInfo.GetDrives();
             foreach (DriveInfo drive in drives)
             {
@@ -31,8 +45,13 @@ namespace oop_lab13
                     Console.WriteLine($"свободное пространство: {drive.TotalFreeSpace}");
                     Console.WriteLine($"метка: {drive.VolumeLabel}");
                 }
+                sw.WriteLine("--------------------------");
+                sw.WriteLine(DateTime.Now);
+                sw.WriteLine("имя диска: {0}", drive.Name);
+                sw.WriteLine("--------------------------");
                 Console.WriteLine();
             }
+            sw.Close();
         }
        
 
@@ -42,7 +61,7 @@ namespace oop_lab13
     {
         public static void FileInfoWrite()
         { 
-           //StreamWriter sw = new StreamWriter(@"D:\Саша Комкова\oop\oop_lab13\kavlog.txt", true);
+            StreamWriter sw = new StreamWriter(@"D:\Саша Комкова\oop\oop_lab13\kavlog.txt", true);
             string path = @"D:\Саша Комкова\oop\oop_lab13\kavlog.txt";
             FileInfo fileInf = new FileInfo(path);
             if (fileInf.Exists)
@@ -54,8 +73,12 @@ namespace oop_lab13
                 Console.WriteLine("время создания: {0}", fileInf.CreationTime);
                 Console.WriteLine("время изменения: {0}", fileInf.LastWriteTime);
 
+                sw.WriteLine("--------------------------");
+                sw.WriteLine(DateTime.Now);
+                sw.WriteLine("полный путь: {0}", fileInf.DirectoryName);
+                sw.WriteLine("--------------------------");
             }
-
+            sw.Close();
         }
 
 
@@ -65,7 +88,7 @@ namespace oop_lab13
     {
         public static void DirInfoWrite()
         {
-           // StreamWriter sw = new StreamWriter(@"D:\Саша Комкова\oop\oop_lab13\kavlog.txt", true);
+            StreamWriter sw = new StreamWriter(@"D:\Саша Комкова\oop\oop_lab13\kavlog.txt", true);
             string dirName = @"D:\Саша Комкова\oop";
             if (Directory.Exists(dirName))
             {
@@ -86,8 +109,13 @@ namespace oop_lab13
                 Console.WriteLine($"родительский каталог: {dirInfo.Parent}");
                 Console.WriteLine($"время создания: {dirInfo.CreationTime}");
 
+                sw.WriteLine("--------------------------");
+                sw.WriteLine(DateTime.Now);
+                sw.WriteLine("полный путь: {0}", dirInfo.Name);
+                sw.WriteLine("--------------------------");
             }
 
+            sw.Close();
         }
 
 
@@ -98,10 +126,12 @@ namespace oop_lab13
     {
         public static void FileManager()
         {
-           // StreamWriter sw = new StreamWriter(@"D:\Саша Комкова\oop\oop_lab13\kavlog.txt", true);
+            StreamWriter sw = new StreamWriter(@"D:\Саша Комкова\oop\oop_lab13\kavlog.txt", true);
             string dirName = @"D:\Саша Комкова\oop\oop_lab13";
             string KAVInspect = @"D:\Саша Комкова\oop\oop_lab13\KAVInspect";
             string KAVFiles = @"D:\Саша Комкова\oop\oop_lab13\KAVFiles";
+            sw.WriteLine("--------------------------");
+            sw.WriteLine(DateTime.Now);
             if (Directory.Exists(dirName))
             {
                 Console.WriteLine("подкаталоги:");
@@ -118,7 +148,8 @@ namespace oop_lab13
                     Console.WriteLine(s);
                 }
                 DirectoryInfo dirNew = new DirectoryInfo(KAVInspect);
-                string KAVdirinfo = @"D:\Саша Комкова\oop\oop_lab13\KAVInspect\KAVdirinfo.txt";
+               // string KAVdirinfo = @"D:\Саша Комкова\oop\oop_lab13\KAVInspect\KAVdirinfo.txt";
+                string KAVdirinfo = Path.Combine(@"D:\Саша Комкова", "KAVdirinfo.txt");
                 string KAVdirinfo_new = @"D:\KAVdirinfo.txt";
                 FileInfo fileInf = new FileInfo(KAVdirinfo);
                 
@@ -136,6 +167,8 @@ namespace oop_lab13
                     fileInf.Delete();
                     dirNew.Delete();
                 }
+                sw.WriteLine("полный путь: {0}", fileInf.DirectoryName);
+                sw.WriteLine("полный путь: {0}", dirNew.Name);
             }
             string KAVcopy_from = @"C:\Users\Саша Комкова\Desktop\UNI\оси\*txt";
             
@@ -152,6 +185,19 @@ namespace oop_lab13
             {
                 dirFiles.MoveTo(KAVInspect);
             }
+            
+            sw.WriteLine("полный путь: {0}", dirFiles.Name);
+            sw.WriteLine("--------------------------");
+            string sourceFolder = @"D:\Саша Комкова\oop\oop_lab13\KAVInspect" ; // исходная папка
+            string zipFile = @"D:\Саша Комкова\oop\oop_lab13\KAVInspect.zip"; // сжатый файл
+            string targetFolder = @"C:\Users\Саша Комкова\Desktop\UNI\ооп"; // папка, куда распаковывается файл
+
+            ZipFile.CreateFromDirectory(sourceFolder, zipFile);
+            Console.WriteLine($"папка {sourceFolder} архивирована в файл {zipFile}");
+            ZipFile.ExtractToDirectory(zipFile, targetFolder);
+
+            Console.WriteLine($"файл {zipFile} распакован в папку {targetFolder}");
+            sw.Close();
         }
         
 
@@ -160,11 +206,12 @@ namespace oop_lab13
     {
         static void Main(string[] args)
         {
-           // KAVDriveInfo.DriveInfoWrite();
-           // KAVFileInfo.FileInfoWrite();
-           // KAVDirInfo.DirInfoWrite();
+           
+            KAVDriveInfo.DriveInfoWrite();
+            KAVFileInfo.FileInfoWrite();
+            KAVDirInfo.DirInfoWrite();
             KAVFileManager.FileManager();
-
+            KAVLog.Log();
 
             Console.ReadKey();
         }
